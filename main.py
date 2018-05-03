5c65f930ae6580220e5637bdfa739476449cea60
Matt Maeda <matt@casamaeda.com>
2018-05-03 12:21:25 -0700

    initial commit for obama speaks the facts

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, render_template, session
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
-
+APP = Flask(__name__)
+URL = "http://talkobamato.me/synthesize.py"
 
 def get_fact():
 
@@ -17,12 +17,19 @@ def get_fact():
     return facts[0].getText()
 
 
-@app.route('/')
+@APP.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    resp = requests.post(URL,
+                         data={"input_text": fact},
+                         allow_redirects=False)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    link = soup.find_all("a")
+    href = link[0]["href"]
+
+    return render_template('obama.jinja2', href=href)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
-
+    PORT = int(os.environ.get("PORT", 6787))
+    APP.run(host='0.0.0.0', port=PORT)
